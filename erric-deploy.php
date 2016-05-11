d4231169d44e0ccf93f645b64577f27edd0620b7
Eric Gunawan <erricgunawan@gmail.com>
2016-05-12 05:05:15 +0700

    bump to 1.1.0

--- erric-deploy.php
+++ erric-deploy.php
@@ -3,7 +3,7 @@
  * Plugin Name: Erric Deploy
  * Plugin URI: https://www.smashingmagazine.com/2015/08/deploy-wordpress-plugins-with-github-using-transients/
  * Description: Erric Playground for Testing Deploy from GitHub
- * Version: 0.8.0
+ * Version: 1.1.0
  * Author: Eric Gunawan
  * Author URI: http://erricgunawan.com
  *
